3d8356367efdf8503b3d3b82dd893e88aba32385
liudehuang <2969878315@qq.com>
2018-04-03 15:48:36 +0800

    解决生成出库单插入订单数组有空格的bug

--- src/main/java/com/djcps/wms/commons/enums/SysMsgEnum.java
+++ src/main/java/com/djcps/wms/commons/enums/SysMsgEnum.java
@@ -95,8 +95,7 @@ public enum SysMsgEnum implements MsgInterface {
 	
 	ALREADY_INTELLIGENT_ALLOCATION(880022,"该配货结果已确认"),
 	NOT_DEAL(880023,"还有任务未处理无法完成装车"),
-	NOT_TASK(880024,"当前没有任务"),
-	OUTORDER_FAIL(880025,"生成出库单失败")
+	NOT_TASK(880024,"当前没有任务")
 	;
 
     private int code;
